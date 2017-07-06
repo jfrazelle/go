9f5be313fc494f385e182d985d84f0a43f7f9c00
Ying Li <ying.li@docker.com>
2017-07-05 19:28:06 -0700

    Update Tonis's patch 3:
    Encode non-fatal floats as integers in canonical mode
    
    Signed-off-by: Ying Li <ying.li@docker.com>

--- canonical/json/encode.go
+++ canonical/json/encode.go
@@ -564,21 +564,25 @@ func (bits floatEncoder) encode(e *encodeState, v reflect.Value, opts encOpts) {
 	// Like fmt %g, but the exponent cutoffs are different
 	// and exponents themselves are not padded to two digits.
 	b := e.scratch[:0]
-	abs := math.Abs(f)
-	fmt := byte('f')
-	// Note: Must use float32 comparisons for underlying float32 value to get precise cutoffs right.
-	if abs != 0 {
-		if bits == 64 && (abs < 1e-6 || abs >= 1e21) || bits == 32 && (float32(abs) < 1e-6 || float32(abs) >= 1e21) {
-			fmt = 'e'
+	if e.canonical {
+		b = strconv.AppendInt(b, int64(f), 10)
+	} else {
+		abs := math.Abs(f)
+		fmt := byte('f')
+		// Note: Must use float32 comparisons for underlying float32 value to get precise cutoffs right.
+		if abs != 0 {
+			if bits == 64 && (abs < 1e-6 || abs >= 1e21) || bits == 32 && (float32(abs) < 1e-6 || float32(abs) >= 1e21) {
+				fmt = 'e'
+			}
 		}
-	}
-	b = strconv.AppendFloat(b, f, fmt, -1, int(bits))
-	if fmt == 'e' {
-		// clean up e-09 to e-9
-		n := len(b)
-		if n >= 4 && b[n-4] == 'e' && b[n-3] == '-' && b[n-2] == '0' {
-			b[n-2] = b[n-1]
-			b = b[:n-1]
+		b = strconv.AppendFloat(b, f, fmt, -1, int(bits))
+		if fmt == 'e' {
+			// clean up e-09 to e-9
+			n := len(b)
+			if n >= 4 && b[n-4] == 'e' && b[n-3] == '-' && b[n-2] == '0' {
+				b[n-2] = b[n-1]
+				b = b[:n-1]
+			}
 		}
 	}
 
